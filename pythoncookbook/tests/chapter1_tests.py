d1f4545d8c7680ae7f3e645417cf029a35b0bc01
Will Keeling <will@zifferent.com>
2017-09-01 20:37:00 +0100

    Tests for finding N smallest/largest items

--- pythoncookbook/tests/chapter1_tests.py
+++ pythoncookbook/tests/chapter1_tests.py
@@ -118,3 +118,38 @@ class KeepingTheLastNItemsTest(TestCase):
                          ['line3', 'line4', 'line5', 'line6', 'line7'])
         self.assertEqual(found['line14 python'],
                          ['line9', 'line10', 'line11', 'line12', 'line13'])
+
+
+class FindingTheLargestOrSmallestNItemsTest(TestCase):
+
+    def test_find_three_largest_items(self):
+        """Hint: do this without using slice syntax."""
+        nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+
+        self.fail('Write a single line variable assignment')
+
+        self.assertEqual(largest, [42, 37, 23])
+
+    def test_find_three_smallest_items(self):
+        """Hint: do this without using slice syntax and do not
+        sort the list.
+        """
+        nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+
+        self.fail('Write a single line variable assignment')
+
+        self.assertEqual(smallest, [-4, 1, 2])
+
+    def test_find_three_largest_items_complex(self):
+        portfolio = [
+            {'name': 'IBM', 'shares': 100, 'price': 91.1},
+            {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+            {'name': 'FB', 'shares': 200, 'price': 21.09},
+            {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+            {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+            {'name': 'ACME', 'shares': 75, 'price': 115.65}
+        ]
+
+        self.fail('Write a single line variable assignment')
+
+        self.assertEqual(expensive, [200, 100, 75])
