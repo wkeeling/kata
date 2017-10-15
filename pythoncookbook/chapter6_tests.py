25f1d9f7c0aa1f6110d29ca4b3c70d4f1fbaebff
Will Keeling <will@zifferent.com>
2017-10-15 12:07:32 +0100

    More chapter6 tests

--- pythoncookbook/chapter6_tests.py
+++ pythoncookbook/chapter6_tests.py
@@ -2,6 +2,7 @@
 
 from collections import namedtuple
 from io import StringIO
+import os
 from unittest import TestCase
 
 from pythoncookbook.code.chapter6 import dict_to_xml
@@ -106,3 +107,142 @@ class TurningADictionaryIntoXMLTest(TestCase):
 
         self.assertEqual(result, b'<stock><price>490.1</price><shares>100'
                                  b'</shares><name>GOOG</name></stock>')
+
+
+class ParsingModifyingAndRewritingXMLTest(TestCase):
+
+    def test_get_root_element(self):
+        self.fail('Parse the file data/pred.xml and get the root element')
+
+        self.assertEqual(root.name, 'stop')
+
+    def test_remove_elements(self):
+        self.fail('Remove the elements <sri> and <cr> from data/pred.xml '
+                  'and write the new XML document to data/pred1.xml')
+
+        with open('data/pred1.xml') as xml:
+            content = xml.read()
+
+        self.assertNotIn('<sri>', content)
+        self.assertNotIn('<cr>', content)
+        os.remove('data/pred1.xml')
+
+    def test_insert_element(self):
+        self.fail('Read data/pred.xml and insert a new element after the '
+                  '<nm> tag called <spam>test</spam> and write the new XML '
+                  'document to data/pred1.xml')
+
+        with open('data/pred1.xml') as f1:
+            content = f1.read()
+
+        with open('data/pred_expected.xml') as f2:
+            expected = f2.read()
+
+        self.assertEqual(f1, f2)
+
+    def test_get_nth_child(self):
+        """Hint: do his without using find or iterating the root node."""
+        self.fail('Read data/pred.xml and extract the 1, 2 and 3rd elements')
+
+        self.assertEqual(child_names, ['nm', 'sri', 'cr'])
+
+
+class InteractingWithARelationalDatabaseTest(TestCase):
+    """The tests in this class a interdependent. They won't work
+    if executed individually.
+    """
+
+    def test_create_database_table(self):
+        """Hint: create a table called portfolio with columns symbol (text),
+        shares (integer), price (real). Use sqlite3 and use a database file
+        of data/test.db. Manually check that the table was created.
+        """
+        self.fail('Create the database table')
+
+    def test_insert_data(self):
+        """Hint: use the same database as created in the previous test,
+        and insert the records into it. Manually check that the records
+        were inserted.
+        """
+        stocks = [
+            ('GOOG', 100, 490.1),
+            ('AAPL', 50, 545.75),
+            ('FB', 150, 7.45),
+            ('HPQ', 75, 33.2),
+        ]
+        self.fail('Insert the specified records')
+
+    def test_read_data(self):
+        """Hint: use the same database as the previous test and read
+        back the records that were previously inserted.
+        """
+        self.fail('Read all the records in the table')
+
+        self.assertEqual(records, [
+            ('GOOG', 100, 490.1),
+            ('AAPL', 50, 545.75),
+            ('FB', 150, 7.45),
+            ('HPQ', 75, 33.2),
+        ])
+
+    def test_read_data_parameterized(self):
+        """Hint: use the same database as the previous test and read
+        back records that have a minimum price of 100."""
+        self.fail('Read records with a price over 100')
+
+        self.assertEqual(records, [
+            ('GOOG', 100, 490.1),
+            ('AAPL', 50, 545.75),
+        ])
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove('data/test.db')
+
+
+class DecodingAndEncodingHexadecimalDigitsTest(TestCase):
+
+    def test_encode_as_hex(self):
+        s = b'hello'
+
+        self.fail('Write a single line expression')
+
+        self.assertEqual(encoded, b'68656c6c6f')
+
+    def test_encode_as_hex_alternate(self):
+        s = b'hello'
+
+        self.fail('Write a single line expression')
+
+        self.assertEqual(encoded, b'68656C6C6F')
+
+    def test_decode_hex(self):
+        h = b'68656c6c6f'
+
+        self.fail('Write a single line expression')
+
+        self.assertEqual(decoded, b'hello')
+
+    def test_decode_hex_alternate(self):
+        h = b'68656C6C6F'
+
+        self.fail('Write a single line expression')
+
+        self.assertEqual(decoded, b'hello')
+
+
+class DecodingAndEncodingBase64Test(TestCase):
+
+    def test_encode_as_base64(self):
+        s = b'hello'
+
+        self.fail('Write a single line expression')
+
+        self.assertEqual(encoded, b'aGVsbG8=')
+
+    def test_decode_base64(self):
+        b = b'aGVsbG8='
+
+        self.fail('Write a single line expression')
+
+        self.assertEqual(decoded, b'hello')
