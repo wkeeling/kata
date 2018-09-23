a5d3d23a5450e8421c1686564efbe0557f16983f
Will Keeling <will@zifferent.com>
2018-09-23 10:23:42 +0100

    Test pass placeholder home page

--- formula1/championship/tests/test_views.py
+++ formula1/championship/tests/test_views.py
@@ -3,7 +3,12 @@ from django.test import TestCase
 
 class HomePageTest(TestCase):
 
-    def test_displays_drivers_championship(self):
+    def test_redirects_to_home_page(self):
         response = self.client.get('/')
+
+        self.assertContains(response, "Formula 1 Championship")
+
+    def test_displays_drivers_championship(self):
+        response = self.client.get('/championship/')
 
         self.assertContains(response, "Formula 1 Championship")
