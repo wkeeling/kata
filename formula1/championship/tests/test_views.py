b5c4c2ea1dddf06b5989e4413b5d9c24c61daafb
Will Keeling <will@zifferent.com>
2018-09-23 10:51:21 +0100

    Test template rendering

--- formula1/championship/tests/test_views.py
+++ formula1/championship/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.template.loader import render_to_string
 from django.test import TestCase
 
 
@@ -8,7 +9,14 @@ class HomePageTest(TestCase):
 
         self.assertEqual(response.status_code, 302)
 
-    def test_displays_drivers_championship(self):
+    def test_uses_correct_template(self):
         response = self.client.get('/championship/')
 
-        self.assertContains(response, "Formula 1 Championship")
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_renders_home_page(self):
+        expected_html = render_to_string('home.html')
+
+        response = self.client.get('/championship/')
+
+        self.assertMultiLineEqual(response.content.decode(), expected_html)
