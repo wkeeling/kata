b6c2a49af4aa76979c0a866a689e2de8bba5faa4
Will Keeling <will@zifferent.com>
2018-10-14 10:47:06 +0100

    Introduce bootstrap

--- formula1/formula1/settings.py
+++ formula1/formula1/settings.py
@@ -124,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    # For the devserver to serve static files
+    os.path.join(BASE_DIR, 'static'),
+]
