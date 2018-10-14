5e3f182eeb613b0dad0ec6652f61ee9805d1fc62
Will Keeling <will@zifferent.com>
2018-10-14 15:03:46 +0100

    Fix test

--- formula1/championship/tests/test_views.py
+++ formula1/championship/tests/test_views.py
@@ -18,7 +18,13 @@ class HomePageTest(TestCase):
 
     def test_renders_home_page(self):
         season = Season.objects.get(year=2017)
-        expected_html = render_to_string('home.html', {'season': season})
+        expected_html = render_to_string('home.html', {
+            'season': season,
+            'navigation': [
+                ('', 'Switch Season'),
+                ('', 'Add Season')
+            ]
+        })
 
         response = self.client.get('/championship/')
 
