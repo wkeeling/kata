06e6c35de1b760dad49ff7876b214912c62e8146
Will Keeling <will@zifferent.com>
2018-10-14 15:02:37 +0100

    Make navigation links view specific

--- formula1/championship/views.py
+++ formula1/championship/views.py
@@ -4,4 +4,10 @@ from championship.models import Season
 
 
 def home_page(request):
-    return render(request, 'home.html', {'season': Season.objects.first()})
+    return render(request, 'home.html', {
+        'season': Season.objects.first(),
+        'navigation': [
+            ('', 'Switch Season'),
+            ('', 'Add Season')
+        ]
+    })
