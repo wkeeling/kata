88c9c2d78caa8ed903a7987ed0b254f41d0cd7ad
Will Keeling <will@zifferent.com>
2019-07-21 19:02:51 +0100

    Remove unused project

--- formula1/championship/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChampionshipConfig(AppConfig):
-    name = 'championship'
--- formula1/championship/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-import championship.views
-
-
-urlpatterns = [
-    path('', championship.views.home_page, name='home'),
-    path('add', championship.views.add_season, name='add_season'),
-]
