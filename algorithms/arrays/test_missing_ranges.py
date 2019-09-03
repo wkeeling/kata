ec0c5596f54f070503ffcb5dd4d1f6689b8c58df
Will Keeling <will.keeling@nanoporetech.com>
2019-09-03 13:02:29 +0100

    Add test

---
+++ algorithms/arrays/test_missing_ranges.py
@@ -0,0 +1,12 @@
+"""
+Find missing ranges between low and high in the given array.
+Ex) [3, 5] lo=1 hi=10 => answer: [(1, 2), (4, 4), (6, 10)]
+"""
+
+
+def missing_ranges(arr, lo, hi):
+    pass
+
+
+def test_missing_ranges():
+    assert list(missing_ranges([3, 5], 1, 10)) == [(1, 2), (4, 4), (6, 10)]
---
+++ algorithms/arrays/test_move_zeros.py
@@ -0,0 +1,15 @@
+"""
+Write an algorithm that takes an array and moves all of the zeros to the end,
+preserving the order of the other elements.
+    move_zeros([false, 1, 0, 1, 2, 0, 1, 3, "a"])
+    returns => [false, 1, 1, 2, 1, 3, "a", 0, 0]
+"""
+
+
+def move_zeros(arr):
+    pass
+
+
+def test_move_zeros():
+    assert move_zeros([False, 1, 0, 1, 2, 0, 1, 3, "a"]) == \
+           [False, 1, 1, 2, 1, 3, "a", 0, 0]
